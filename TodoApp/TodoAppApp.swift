49ecbe799cb629217ac747142858997164afec8f
Anup Saud <anupsaud10@gmail.com>
2024-09-10 22:03:43 -0400

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Anup Saud on 2024-09-10.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
